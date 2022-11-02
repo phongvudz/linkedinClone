c270ae78bd20a9be51028272fee213c51069f9f0
Phong Vu <nguyennguyen969873@gmail.com>
2022-11-02 12:58:54 +0700

    fix

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -45,10 +45,6 @@ const Login = () => {
 
   return (
     <div className="login">
-      <img
-        src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c528.png"
-        alt="logo"
-      />
       <form>
         <input
           value={name}
